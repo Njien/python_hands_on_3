1a894249bfb2bae6828dbc2bb9fb22b39dac5dbe
Njien <thomasjessyendi@gmail.com>
2025-07-24 16:33:38 +0100

    Task 3 first commit

--- task3.py
+++ task3.py
@@ -10,3 +10,14 @@ money = [1000, 1200, 800, 1500, 1100]
 
 → Perform the corrections and computations, and print all results.
 """
+
+money = [1000, 1200, 800, 1500, 1100]
+print(money)
+sum_money = money[0] + money [1] + money[2] + money[3] + money[4]
+
+print(f"the sum of the money is: {sum_money:,.2f}")
+
+money[2] = 1000
+print(money)
+
+print("The reverse order of the list is: ", money[::-1])
